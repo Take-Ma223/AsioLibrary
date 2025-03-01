2d8d1699bff90750b495db9818ab8f9349c98c95
TakeMa <66286399+Take-Ma223@users.noreply.github.com>
2025-03-02 04:17:32 +0900

    最初のコミット

---
+++ AsioLibrary.cs
@@ -0,0 +1,33 @@
+﻿using NAudio.Wave;
+using System.Runtime.InteropServices;
+
+
+public class AsioLibrary
+{
+    [UnmanagedCallersOnly(EntryPoint = "GetAsioDriverNames")]
+    public static IntPtr GetAsioDriverNames()
+    {
+        try
+        {
+            var drivers = AsioOut.GetDriverNames();
+            string result = string.Join("\n", drivers);
+
+            // 文字列をネイティブメモリに格納
+            IntPtr buffer = Marshal.StringToHGlobalAnsi(result);
+            return buffer;
+        }
+        catch (Exception)
+        {
+            return IntPtr.Zero;
+        }
+    }
+
+    [UnmanagedCallersOnly(EntryPoint = "FreeMemory")]
+    public static void FreeMemory(IntPtr ptr)
+    {
+        if (ptr != IntPtr.Zero)
+        {
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+}
